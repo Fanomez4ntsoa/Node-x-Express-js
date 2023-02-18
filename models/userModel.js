5b5bd03b74f216f8d3807864d33b95353b7027e0
Fanomez4ntsoa <87415578+Fanomez4ntsoa@users.noreply.github.com>
2023-02-18 21:03:47 +0300

    cmd : installation package : express-async-handler & jsonwebtoken

--- models/userModel.js
+++ models/userModel.js
@@ -24,6 +24,10 @@ var userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+  },
+  role: {
+    type: String,
+    default: "user"
   }
 });
 
@@ -31,6 +35,9 @@ userSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSaltSync(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
 
 // Export the model
 module.exports = mongoose.model("User", userSchema);

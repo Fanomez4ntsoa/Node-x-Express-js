23dcc272b040c48e523e0f1ed988f694dd7a8e90
Fanomez4ntsoa <87415578+Fanomez4ntsoa@users.noreply.github.com>
2023-02-23 22:26:35 +0300

    hotfix: route Category

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,8 +4,8 @@ const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get('/', getAllCategories);
+router.post('/new-category', authMiddleware, isAdmin, createCategory);
 router.put('/edit-category/:id', authMiddleware, isAdmin, updateCategory);
 router.delete('/:id', authMiddleware, isAdmin, deleteCategory);
-router.post('/new-category', authMiddleware, isAdmin, createCategory);
 
 module.exports = router;
